7471b9a5d1a5d1a29fb2cd12f770b8b43522e170
Eiolv <akopylov95@mail.ru>
2019-01-24 13:36:06 +0300

    Готовое ДЗ

---
+++ Lesson5/src/Employee.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+
+public class Employee {
+    String fio;
+    String position;
+    String email;
+    String phone;
+    Integer salaries;
+    Integer age;
+
+    public Employee(String _fio, String _position, String _email, String _phone, Integer _salaries, Integer _age) {
+        fio = _fio;
+        position = _position;
+        email = _email;
+        phone = _phone;
+        salaries = _salaries;
+        age = _age;
+    }
+
+    public void run(){
+
+        System.out.println("ФИО: " + fio);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Номер телефона: " + phone);
+        System.out.println("Размер заработной платы: " + salaries);
+        System.out.println("Возраст: " + age);
+        System.out.println();
+    }
+}
